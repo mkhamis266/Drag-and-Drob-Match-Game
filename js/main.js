f9387cd8bdf8889b9f550d23f006b3691484e872
Mahmoud Khamis Ahmed <68229221+mkhamis266@users.noreply.github.com>
2023-08-17 11:02:48 +0300

    ui changes

--- js/main.js
+++ js/main.js
@@ -1,82 +1,82 @@
 /* put your medicines info here */
 let medicines = [
   {
-    mName: "m1",
+    mName: "medicine-1",
     mImgSrc: "imgs/p1.png",
   },
   {
-    mName: "m2",
+    mName: "medicine-2",
     mImgSrc: "imgs/p2.png",
   },
   {
-    mName: "m3",
+    mName: "medicine-3",
     mImgSrc: "imgs/p3.png",
   },
 ];
 
 /* put your medicines info here */
-/* NOTE:- 
-* bactria medicine value should be equal to medicine mName value in medicines array
-*/
+/* NOTE:-
+ * bactria medicine value should be equal to medicine mName value in medicines array
+ */
 let bactria = [
   {
     bName: "b1",
-    medicine: "m1",
+    medicine: "medicine-1",
     bImgSrc: "imgs/bacteria-1.png",
   },
   {
     bName: "b2",
-    medicine: "m2",
+    medicine: "medicine-2",
     bImgSrc: "imgs/bacteria-2.png",
   },
   {
     bName: "b3",
-    medicine: "m3",
+    medicine: "medicine-3",
     bImgSrc: "imgs/bacteria-3.png",
   },
   {
     bName: "b1",
-    medicine: "m1",
+    medicine: "medicine-1",
     bImgSrc: "imgs/bacteria-1.png",
   },
   {
     bName: "b2",
-    medicine: "m2",
+    medicine: "medicine-2",
     bImgSrc: "imgs/bacteria-2.png",
   },
   {
     bName: "b3",
-    medicine: "m3",
+    medicine: "medicine-3",
     bImgSrc: "imgs/bacteria-3.png",
   },
   {
     bName: "b1",
-    medicine: "m1",
+    medicine: "medicine-1",
     bImgSrc: "imgs/bacteria-1.png",
   },
   {
     bName: "b2",
-    medicine: "m2",
+    medicine: "medicine-2",
     bImgSrc: "imgs/bacteria-2.png",
   },
   {
     bName: "b3",
-    medicine: "m3",
+    medicine: "medicine-3",
     bImgSrc: "imgs/bacteria-3.png",
   },
   {
     bName: "b1",
-    medicine: "m1",
+    medicine: "medicine-1",
     bImgSrc: "imgs/bacteria-1.png",
   },
   {
     bName: "b2",
-    medicine: "m2",
+    medicine: "medicine-2",
     bImgSrc: "imgs/bacteria-2.png",
   },
   {
     bName: "b3",
-    medicine: "m3",
+    medicine: "medicine-3",
     bImgSrc: "imgs/bacteria-3.png",
   },
 ];
@@ -89,18 +89,18 @@ $(init);
 function init() {
   // Hide the success message
   $("#successMessage").hide();
-  $("#successMessage").css({
-    left: "580px",
-    top: "250px",
-    width: 0,
-    height: 0,
-  });
+  // $("#successMessage").css({
+  //   left: "580px",
+  //   top: "250px",
+  //   width: 0,
+  //   height: 0,
+  // });
 
   // Reset the game
   correctCards = 0;
-  $("#cardPile").html("");
-  $("#cardSlots").html("");
-  $("#scoreBoard .row").html("")
+  $("#cardPile .content").html("");
+  $("#cardSlots .content").html("");
+  $("#scoreBoard .row").html("");
 
   // Create the pile of shuffled cards
   medicines.sort(function () {
@@ -112,7 +112,7 @@ function init() {
       .html(`<img src="${medicines[i].mImgSrc}" class="img-fluid"> `)
       .data("medicine", medicines[i].mName)
       .attr("id", "card" + medicines[i].mName)
-      .appendTo("#cardPile")
+      .appendTo("#cardPile .content")
       .draggable({
         stack: "#cardPile div",
         cursor: "move",
@@ -123,9 +123,9 @@ function init() {
     $("<div/>")
       .html(
         `
-        <div id="${medicines[i].mName}Score" class="text-center">
-            <h4 class="text-danger">${medicines[i].mName}</h4>
-            <p>0</p>
+        <div id="${medicines[i].mName}Score" class="d-flex justify-content-around flex-wrap text-white">
+            <h4>${medicines[i].mName}: </h4>
+            <p><span class="points">0</span> Points</p>
         </div>
       `
       )
@@ -138,7 +138,7 @@ function init() {
     return Math.random() - 0.5;
   });
   for (let i = 0; i < bactria.length; i++) {
-    $("<div/>").html(`<img src="${bactria[i].bImgSrc}" class="img-fluid"> `).data("medicine", bactria[i].medicine).addClass("col-1").appendTo("#cardSlots").droppable({
+    $("<div/>").html(`<img src="${bactria[i].bImgSrc}" class="img-fluid"> `).data("medicine", bactria[i].medicine).addClass("col-1").appendTo("#cardSlots .content").droppable({
       accept: "#cardPile div",
       hoverClass: "hovered",
       drop: handleCardDrop,
@@ -160,8 +160,8 @@ function handleCardDrop(event, ui) {
     // ui.draggable.draggable( 'disable' );
     $(this).droppable("disable");
     $(this).animate({ opacity: 0 }, 500);
-    let medicineScore = Number($(`#${cardMedicineName}Score p`).html());
-    $(`#${cardMedicineName}Score p`).html(medicineScore+1);
+    let medicineScore = Number($(`#${cardMedicineName}Score .points`).html());
+    $(`#${cardMedicineName}Score .points`).html(medicineScore + 1);
     // ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
     // ui.draggable.draggable( 'option', 'revert', false );
     correctCards++;
@@ -173,10 +173,6 @@ function handleCardDrop(event, ui) {
   if (correctCards == bactria.length) {
     $("#successMessage").show();
     $("#successMessage").animate({
-      left: "380px",
-      top: "200px",
-      width: "400px",
-      height: "100px",
       opacity: 1,
     });
   }
